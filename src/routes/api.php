c6e0ce0daa4e6da59588a4fbaf0fb75ac14f2f4a
Bohdan Suprun <bohdan-suprun@outlook.com>
2024-09-19 11:41:26 +0000

    fixed route

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use IpCountryDetector\Http\Controllers\IPCheckController;
 
-Route::middleware(config('ipcountry.middleware'))
-    ->group(function () {
-        Route::match(['get', 'post'], config('ipcountry.route'), [IPCheckController::class, 'checkIP']);
-    });
+Route::match(['get', 'post'], config('ipcountry.route'), [IPCheckController::class, 'checkIP']);
