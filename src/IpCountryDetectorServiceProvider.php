f0c1aedd306535ec2becd33ce031face8c986f1a
Bohdan Suprun <bohdan-suprun@outlook.com>
2024-09-19 09:44:56 +0000

    Removed oauth

--- src/IpCountryDetectorServiceProvider.php
+++ src/IpCountryDetectorServiceProvider.php
@@ -5,16 +5,10 @@ namespace IpCountryDetector;
 use Illuminate\Support\ServiceProvider;
 use IpCountryDetector\Http\Controllers\IPCheckController;
 use IpCountryDetector\Services\IPCheckService;
-use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Rsa\Sha256;
-use Lcobucci\JWT\Signer\Key\InMemory;
 use IpCountryDetector\Console\InstallIpCountryDetectorCommand;
-use IpCountryDetector\Http\Middleware\IpAuthorization;
 use IpCountryDetector\Services\Interfaces\ErrorHandlerInterface;
 use IpCountryDetector\Services\Interfaces\IpCountryServiceInterface;
-use IpCountryDetector\Services\Interfaces\JWTServiceInterface;
 use IpCountryDetector\Services\IpApiService;
-use IpCountryDetector\Services\JWTService;
 use IpCountryDetector\Services\ErrorHandlerService;
 
 class IpCountryDetectorServiceProvider extends ServiceProvider
@@ -25,29 +19,10 @@ class IpCountryDetectorServiceProvider extends ServiceProvider
             __DIR__ . '/config/ipcountry.php', 'ipcountry'
         );
 
-        $this->app->singleton(Configuration::class, function ($app) {
-            $publicKeyPath = config('ipcountry.keys.public');
-            $privateKeyPath = config('ipcountry.keys.private');
-
-            $publicKey = InMemory::file($publicKeyPath);
-            $privateKey = InMemory::file($privateKeyPath);
-
-            return Configuration::forAsymmetricSigner(
-                new Sha256(),
-                $privateKey,
-                $publicKey
-            );
-        });
-
-        $this->app->singleton(JWTService::class, function ($app) {
-            return new JWTService($app->make(Configuration::class));
-        });
-
         $this->app->singleton(ErrorHandlerService::class, function ($app) {
             return new ErrorHandlerService();
         });
 
-        $this->app->bind(JWTServiceInterface::class, JWTService::class);
         $this->app->bind(ErrorHandlerInterface::class, ErrorHandlerService::class);
         $this->app->bind(IpCountryServiceInterface::class, IpApiService::class);
 
@@ -71,6 +46,5 @@ class IpCountryDetectorServiceProvider extends ServiceProvider
         }
 
         $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
-        $this->app['router']->aliasMiddleware('ip.authorization', IpAuthorization::class);
     }
 }
--- src/config/ipcountry.php
+++ src/config/ipcountry.php
@@ -1,24 +1,12 @@
 <?php
-
-use IpCountryDetector\Http\Middleware\IpAuthorization;
-
 return [
-    'auth_key' => env('IPCOUNTRY_AUTH_KEY', 'client_auth_key_123'),
-    'auth_enabled' => env('IPCOUNTRY_AUTH_ENABLED', false),
     'route' => '/ip-country',
-    'middleware' => [IpAuthorization::class],
     'redis' => [
         'host' => env('IPCOUNTRY_REDIS_HOST', '127.0.0.1'),
         'port' => env('IPCOUNTRY_REDIS_PORT', 6379),
-        'database' => env('IPCOUNTRY_REDIS_DATABASE', ''),
-        'password' => env('IPCOUNTRY_REDIS_PASSWORD', ''),
+        'database' => env('IPCOUNTRY_REDIS_DATABASE', null),
+        'password' => env('IPCOUNTRY_REDIS_PASSWORD', null),
         'prefix' => env('IPCOUNTRY_REDIS_PREFIX', 'ip_country'),
     ],
-    'secret' => env('JWT_SECRET', 'jwtsecret'),
-    'keys' => [
-        'public' => env('IPCOUNTRY_JWT_PUBLIC_KEY_PATH', storage_path('oauth-public.key')),
-        'private' => env('IPCOUNTRY_JWT_PRIVATE_KEY_PATH', storage_path('oauth-private.key')),
-    ],
-    'ttl' => 60,
-    'algo' => 'RS256',
 ];
+
