e66a38b8d923a7db89a344f4951b8747945aadc4
Bohdan Suprun <bohdan-suprun@outlook.com>
2024-10-31 10:27:55 +0000

    fixed structure

--- src/Http/Controllers/IPCheckController.php
+++ src/Http/Controllers/IPCheckController.php
@@ -29,63 +29,29 @@ class IPCheckController extends Controller
 
         $timeZone = $request->input('timezone', 'UTC');
         $countryStatus = CountryStatus::UNKNOWN;
+        $country = null;
 
         try {
-            if ($ipAddress) {
+            if (empty($ipAddress) || $ipAddress === '127.0.0.1' || $ipAddress === '::1') {
+                Log::info('Local IP or missing IP detected, using timezone and country fallback.');
+                $country = $this->ipCheckService->timeZoneToCountry($timeZone);
+                $countryStatus = CountryStatus::IP_NOT_IN_RANGE;
+            } else {
                 $country = $this->ipCheckService->ipToCountry($ipAddress, $timeZone);
                 $countryStatus = CountryStatus::SUCCESS;
-            } else {
-                $country = null;
-                $countryStatus = CountryStatus::IP_NOT_IN_RANGE;
             }
         } catch (\Exception $e) {
             Log::warning("IP to Country failed, switching to timezone: {$e->getMessage()}");
-            $country = null;
+            $country = $this->ipCheckService->timeZoneToCountry($timeZone);
             $countryStatus = CountryStatus::NOT_FOUND;
-        }
-
-        if (!$country) {
-            try {
-                $country = $this->ipCheckService->timeZoneToCountry($timeZone);
-            } catch (\Exception $e) {
-                Log::warning("TimeZone to Country failed: {$e->getMessage()}");
-                $country = 'Unknown';
-                $countryStatus = CountryStatus::NOT_FOUND;
-            }
-        }
-
-        if (empty($ipAddress) || $ipAddress === '127.0.0.1') {
-            Log::info('Local IP or missing IP detected, using timezone and country fallback.');
-            return [
-                'timezone' => $timeZone,
-                'country' => $country,
-                'status' => $countryStatus->value
-            ];
         }
 
         return [
             'ip' => $ipAddress,
             'timezone' => $timeZone,
-            'country' => $country,
+            'country' => $country ?? 'Unknown',
             'status' => $countryStatus->value
         ];
     }
 
-
-    /**
-     * @throws Exception
-     */
-    public function checkIPFromEntry($ipAddress): ?string
-    {
-        if ($ipAddress == '127.0.0.1' || $ipAddress == '::1') {
-            return null;
-        }
-        $country = $this->ipCheckService->ipToCountrySimple($ipAddress);
-
-        if (empty($country)) {
-            return null;
-        }
-
-        return $country;
-    }
 }
