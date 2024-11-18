4a2c5e842d3f2a4174f7e2bf7607cf63d60d696c
Bohdan Suprun <bohdan-suprun@outlook.com>
2024-11-18 15:39:02 +0000

    Fxied

--- src/database/seeders/IpCountrySeeder.php
+++ src/database/seeders/IpCountrySeeder.php
@@ -45,70 +45,57 @@ class IpCountrySeeder extends Seeder
         }
 
         try {
-            DB::transaction(function () use ($handle) {
-                $dataRows = [];
-                $rowCount = 1;
-                $batchSize = 1000;
-                $batch = [];
+            $dataRows = [];
+            $rowCount = 1;
 
-                while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-                    $dataRows[] = $data;
-                }
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                $dataRows[] = $data;
+            }
 
-                usort($dataRows, function ($a, $b) {
-                    return strcmp($a[2], $b[2]);
-                });
-
-                $totalRows = count($dataRows);
-
-                foreach ($dataRows as $data) {
-                    [$firstIp, $lastIp, $country, $region, $subregion, $city, , $latitude, $longitude, $timezone] = $data;
-
-                    $batch[] = [
-                        'first_ip' => $this->convertIpToNumeric($firstIp),
-                        'last_ip' => $this->convertIpToNumeric($lastIp),
-                        'country' => $country,
-                        'region' => $region,
-                        'subregion' => $subregion,
-                        'latitude' => $latitude,
-                        'longitude' => $longitude,
-                        'city' => $city,
-                        'timezone' => $timezone,
-                    ];
-
-                    if (count($batch) >= $batchSize) {
-                        IpCountry::insertOrIgnore($batch);
-                        $batch = [];
-                    }
-
-                    $percentage = number_format(($rowCount / $totalRows) * 100, 1);
-
-                    $this->logMessage('info', sprintf(
-                        "[%6.1f%% | %6d / 100%% | %6d] - Country: [%2s] - IP Range: [%15s - %-15s] - Region: [%s] - Subregion: [%s] - City: [%s] Map: [%s : %s] - Timezone: [%s]",
-                        $percentage,
-                        $rowCount,
-                        $totalRows,
-                        $country,
-                        str_pad($firstIp, 15, " ", STR_PAD_RIGHT),
-                        str_pad($lastIp, 15, " ", STR_PAD_RIGHT),
-                        $region,
-                        $subregion,
-                        $city,
-                        $latitude,
-                        $longitude,
-                        $timezone
-                    ));
-
-                    $rowCount++;
-                }
-
-                if (!empty($batch)) {
-                    IpCountry::insertOrIgnore($batch);
-                }
-
-                fclose($handle);
-                $this->logMessage('info', "CSV processing completed and file closed.");
+            usort($dataRows, function ($a, $b) {
+                return strcmp($a[2], $b[2]);
             });
+
+            $totalRows = count($dataRows);
+
+            foreach ($dataRows as $data) {
+                [$firstIp, $lastIp, $country, $region, $subregion, $city, , $latitude, $longitude, $timezone] = $data;
+
+                $record = [
+                    'first_ip' => $this->convertIpToNumeric($firstIp),
+                    'last_ip' => $this->convertIpToNumeric($lastIp),
+                    'country' => $country,
+                    'region' => $region,
+                    'subregion' => $subregion,
+                    'latitude' => $latitude,
+                    'longitude' => $longitude,
+                    'city' => $city,
+                    'timezone' => $timezone,
+                ];
+
+                IpCountry::insertOrIgnore($record);
+
+                $this->logMessage('info', sprintf(
+                    "[%6.1f%% | %6d / 100%% | %6d] - Country: [%2s] - IP Range: [%15s - %-15s] - Region: [%s] - Subregion: [%s] - City: [%s] Map: [%s : %s] - Timezone: [%s]",
+                    number_format(($rowCount / $totalRows) * 100, 1),
+                    $rowCount,
+                    $totalRows,
+                    $country,
+                    str_pad($firstIp, 15, " ", STR_PAD_RIGHT),
+                    str_pad($lastIp, 15, " ", STR_PAD_RIGHT),
+                    $region,
+                    $subregion,
+                    $city,
+                    $latitude,
+                    $longitude,
+                    $timezone
+                ));
+
+                $rowCount++;
+            }
+
+            fclose($handle);
+            $this->logMessage('info', "CSV processing completed and file closed.");
         } catch (Throwable $e) {
             $this->logMessage('error', "Failed to process CSV file: {$e->getMessage()}");
         }
